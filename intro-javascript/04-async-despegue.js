3d4dfd7045992ed85664970ae67d05e6b5f8c522
Facundo Montenegro <facumonte29@gmail.com>
2023-04-06 19:38:11 -0300

    termina despegue.js

--- intro-javascript/04-async-despegue.js
+++ intro-javascript/04-async-despegue.js
@@ -5,10 +5,14 @@ const lanzarExcepcion = () => {
     throw new Error('Error inesperado');
 }
 
+const obtenerNumeroAleatorio = (min, max) => Math.floor((Math.random() * (max - min + 1)) + min);
+
 async function etapa(nroEtapa, tiempo) {
     return new Promise((res, rej) => {
         console.log(`Etapa #${nroEtapa} iniciada a las ${getHoraActual()}`);
-        
+
+        setTimeout(lanzarExcepcion, obtenerNumeroAleatorio(4000,16000))
+
         setTimeout(() => {
             res(console.log(`Etapa #${nroEtapa} finalizada a las ${getHoraActual()}`))
         }, tiempo);
@@ -16,12 +20,21 @@ async function etapa(nroEtapa, tiempo) {
 }
 
 const main = async () => {
-    await etapa(1, 2000);
-    await etapa(2,3000);
-    await etapa(3,5000);
-    await etapa(4,6000);
+
+    try {
+        await etapa(1, 2000);
+        await etapa(2, 3000);
+        await etapa(3, 5000);
+        await etapa(4, 6000);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 
 main();
+
+
+
+
 
